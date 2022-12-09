1e1e869692a3ab07af9c28b7686ad5fe97e934b6
Tomáš Voslař <t.voslar@gmail.com>
2022-12-09 12:34:58 +0100

    Revert

--- apollo-router/src/axum_factory/handlers.rs
+++ apollo-router/src/axum_factory/handlers.rs
@@ -21,7 +21,6 @@ use tower::util::BoxService;
 use tower::BoxError;
 use tower::ServiceExt;
 use tower_service::Service;
-use tracing::Instrument;
 
 use super::utils::accepts_json;
 use super::utils::accepts_multipart;
@@ -140,11 +139,7 @@ where
 {
     let (head, body) = http_request.into_parts();
     let mut req: SupergraphRequest = Request::from_parts(head, body).into();
-    req = match apq
-        .apq_request(req)
-        .instrument(tracing::info_span!("apq"))
-        .await
-    {
+    req = match apq.apq_request(req).await {
         Ok(req) => req,
         Err(res) => {
             let (parts, mut stream) = res.response.into_parts();
